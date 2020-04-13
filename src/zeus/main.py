c8786ef8b7f26d70e77e2c549bfb7b216fe3df62
Alistair Hennessey <al@Alistairs-MBP.home>
2020-04-14 00:09:04 +0100

    init

---
+++ src/zeus/main.py
@@ -0,0 +1,23 @@
+import zmq
+
+def main ():
+
+    # Start child threads
+    ctx = zmq.Context()
+    subscriber = ctx.socket(zmq.XSUB)
+    print("Going to bind to sub")
+    subscriber.bind("tcp://*:6001")
+
+    publisher = ctx.socket(zmq.XPUB)
+    print("Going to bind to pub")
+    publisher.bind("tcp://*:6000")
+
+    zmq.proxy(subscriber, publisher)
+
+    # We never get here…
+    subscriber.close()
+    publisher.close()
+    ctx.term()
+
+if __name__ == '__main__':
+    main()
